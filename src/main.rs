fee72bed9965919061e886a66c03024dbb3089ea
rufeng.wei <rufeng.wei@seamoney.com>
2024-08-13 20:48:01 +0800

    feat(main): hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn greet_world() {
+    let southern_germany = "Grüß Gott!";
+    let chinese = "世界，你好";
+    let english = "World, hello";
+    let regions = [southern_germany, chinese, english];
+    // for region in regions.iter() {
+    //     println!("{}", &region);
+    // }
+    // 2021 edition 版本之后，可以直接 for in regions.
+    for region in regions {
+        println!("{}", &region);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    greet_world();
 }
